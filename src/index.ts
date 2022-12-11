23a7a08e648d6c281fce7cafe25671f9c0305c74
Tao <magicdawn@qq.com>
2022-12-11 20:09:18 +0800

    feat: export more

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { notImplemented } from './helper'
 
 type ISupportedBrowsers = IChromiumBasedBrowser | 'firefox' | 'safari'
 
+export { chromiumBasedBrowsers }
+export { ISupportedBrowsers, IChromiumBasedBrowser }
+
 export function readBrowserCookies(
   browser: ISupportedBrowsers = 'chrome',
   options?: { site?: string; profile?: string; keyring_name?: string }
