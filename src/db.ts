1e78d13f2511bb94aad906c9d8f884f687cd100f
Tao <magicdawn@qq.com>
2022-12-11 18:45:14 +0800

    feat: use sql parameters

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
-import { Database, OPEN_READONLY } from 'sqlite3'
+import sqlite3, { Database, OPEN_READONLY } from 'sqlite3'
 import { baseDebug } from './helper'
 
 const DEFAULT_SQL = `
@@ -18,22 +18,23 @@ export type ICookieRow = {
 }
 
 export async function query(dbpath: string, site?: string) {
-  const db = new Database(dbpath, OPEN_READONLY)
+  const db = new Database(dbpath, sqlite3.OPEN_READONLY)
 
   let sql = DEFAULT_SQL
+  const params: Record<string, any> = {}
   if (site) {
-    sql += `WHERE host_key like '%${site}%'`
+    sql += `WHERE host_key like $site`
+    params.$site = `%${site}%`
   }
-  debug('exec sql: %s', sql)
+  debug('exec: sql -> %s, with params -> %o', sql, params)
 
   const rows: ICookieRow[] = await new Promise((resolve, reject) => {
-    db.all(sql, (err, rows) => {
+    db.all(sql, params, (err, rows) => {
       if (err) return reject(err)
       resolve(rows)
     })
   })
 
   db.close()
-
   return rows
 }
